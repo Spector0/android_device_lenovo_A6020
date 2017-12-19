5e10d64a79e898494196a88f22486e76583c587e
Spector <33796688+Spector0@users.noreply.github.com>
2017-12-19 10:18:18 +0530

    Update and rename full_A6020.mk to full_passion.mk

--- full_passion.mk
+++ full_passion.mk
@@ -20,11 +20,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from A6020 device
-$(call inherit-product, device/lenovo/A6020/device.mk)
+$(call inherit-product, device/lenovo/passion/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := A6020
-PRODUCT_NAME := full_A6020
+PRODUCT_DEVICE := passion
+PRODUCT_NAME := full_passion
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Vibe K5
+PRODUCT_MODEL := Vibe P1
 PRODUCT_MANUFACTURER := Lenovo
